593c13c05736f487027eea14ce5b24841cc31442
damian_zalewskipl@o2.pl <damian_zalewskipl@o2.pl>
2019-05-30 18:20:10 +0200

    added quiz data from cloud firestore

--- app/src/main/java/com/example/biennale_go/QuizActivity.java
+++ app/src/main/java/com/example/biennale_go/QuizActivity.java
@@ -2,27 +2,57 @@ package com.example.biennale_go;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class QuizActivity extends AppCompatActivity {
-    private Button summaryButton;
+    private TextView questionNumberTextView;
+    private TextView questionDescriptionTextView;
+    private Button questionAnswerA;
+    private Button questionAnswerB;
+    private Button questionAnswerC;
+    private Button questionAnswerD;
+
+    private Integer questionNumber = 1;
+    private Integer maxQuestionNumber;
+    private ArrayList questions;
+    private Bundle b;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
-        summaryButton = (Button) findViewById(R.id.summaryButton);
-        summaryButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openQuizSummaryActivity();
-            }
-        });
+
+        questionNumberTextView = (TextView) findViewById(R.id.questionNumberTextView);
+        questionDescriptionTextView = (TextView) findViewById(R.id.questionDescriptionTextView);
+        questionAnswerA = (Button) findViewById(R.id.answerA);
+        questionAnswerB = (Button) findViewById(R.id.answerB);
+        questionAnswerC = (Button) findViewById(R.id.answerC);
+        questionAnswerD = (Button) findViewById(R.id.answerD);
+
+        b = getIntent().getExtras();
+
+        if (b != null) {
+            questions = new ArrayList((ArrayList) b.getSerializable("questions"));
+            maxQuestionNumber = questions.size();
+            fillQuestion();
+        }
     }
-    public void openQuizSummaryActivity(){
-        Intent intent = new Intent(this, QuizSummaryActivity.class);
-        startActivity(intent);
+
+
+    private void fillQuestion() {
+        HashMap<String, String> question = new HashMap<>((HashMap<String, String>) questions.get(questionNumber));
+        questionNumberTextView.setText("Pytanie nr" + (questionNumber + 1));
+        questionDescriptionTextView.setText(question.get("description"));
+        questionAnswerA.setText(question.get("answerA"));
+        questionAnswerB.setText(question.get("answerB"));
+        questionAnswerC.setText(question.get("answerC"));
+        questionAnswerD.setText(question.get("answerD"));
     }
 }
--- app/src/main/java/com/example/biennale_go/QuizListActivity.java
+++ app/src/main/java/com/example/biennale_go/QuizListActivity.java
@@ -4,25 +4,21 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class QuizListActivity extends AppCompatActivity {
     private LinearLayout quizListPanel;
@@ -44,18 +40,29 @@ public class QuizListActivity extends AppCompatActivity {
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 findViewById(R.id.loadingPanel).setVisibility(View.GONE);
                 if (task.isSuccessful()) {
-                    Integer k = 0;
-                    for (QueryDocumentSnapshot document : task.getResult()) {
+                    for (final QueryDocumentSnapshot document : task.getResult()) {
                         newButton = new Button(QuizListActivity.this);
                         newButton.setText(document.getData().get("name").toString());
                         newButton.setClickable(true);
                         newButton.setGravity(Gravity.CENTER);
+                        newButton.setOnClickListener(new View.OnClickListener()
+                        {
+                            @Override
+                            public void onClick(View v) {
+                                Intent intent = new Intent(QuizListActivity.this, QuizActivity.class);
+                                Bundle b = new Bundle();
+                                ArrayList questions = (ArrayList) document.getData().get("questions");
+                                b.putString("name", document.getData().get("name").toString());
+                                b.putSerializable("questions", questions);
+                                intent.putExtras(b);
+                                startActivity(intent);
+                            }
+                        });
                         quizListPanel.addView(newButton);
                         // TODO FIX MARGINS
                         newButton = new Button(QuizListActivity.this);
                         newButton.setVisibility(View.INVISIBLE);
                         quizListPanel.addView(newButton);
-                        k = k+1;
                     }
                 } else {
                     Log.d(TAG, "Error getting documents: ", task.getException());
@@ -64,4 +71,6 @@ public class QuizListActivity extends AppCompatActivity {
             }
         });
     }
+
+
 }
