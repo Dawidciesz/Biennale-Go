7c2a0771a535dc930cf4e5fd65172f1b4fd57d37
damian_zalewskipl@o2.pl <damian_zalewskipl@o2.pl>
2019-05-31 11:36:52 +0200

    Dodanie przechodzenia pomiedzy pytaniami oraz wyswietlenie wyniku na widoku podsumowania.

--- app/src/main/java/com/example/biennale_go/QuizActivity.java
+++ app/src/main/java/com/example/biennale_go/QuizActivity.java
@@ -2,6 +2,7 @@ package com.example.biennale_go;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -19,10 +20,11 @@ public class QuizActivity extends AppCompatActivity {
     private Button questionAnswerC;
     private Button questionAnswerD;
 
-    private Integer questionNumber = 1;
+    private Integer questionNumber = 0;
     private Integer maxQuestionNumber;
     private ArrayList questions;
     private Bundle b;
+    private Integer points = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,19 +42,62 @@ public class QuizActivity extends AppCompatActivity {
 
         if (b != null) {
             questions = new ArrayList((ArrayList) b.getSerializable("questions"));
-            maxQuestionNumber = questions.size();
+            maxQuestionNumber = questions.size()-1;
             fillQuestion();
         }
+
+        questionAnswerA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer("a");
+            }
+        });
+        questionAnswerB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer("b");
+            }
+        });
+        questionAnswerC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer("c");
+            }
+        });
+        questionAnswerD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer("d");
+            }
+        });
     }
 
 
     private void fillQuestion() {
         HashMap<String, String> question = new HashMap<>((HashMap<String, String>) questions.get(questionNumber));
-        questionNumberTextView.setText("Pytanie nr" + (questionNumber + 1));
+        questionNumberTextView.setText("Pytanie nr " + (questionNumber + 1));
         questionDescriptionTextView.setText(question.get("description"));
         questionAnswerA.setText(question.get("answerA"));
         questionAnswerB.setText(question.get("answerB"));
         questionAnswerC.setText(question.get("answerC"));
         questionAnswerD.setText(question.get("answerD"));
+    }
+
+    private void checkAnswer(String answer) {
+        HashMap<String, String> question = new HashMap<>((HashMap<String, String>) questions.get(questionNumber));
+        if (answer.equals(question.get("correct"))) {
+            points++;
+        }
+        if(!questionNumber.equals(maxQuestionNumber)) {
+            questionNumber++;
+            fillQuestion();
+        } else {
+            Intent intent = new Intent(QuizActivity.this, QuizSummaryActivity.class);
+            Bundle b2 = new Bundle();
+            b2.putInt("points", points);
+            b2.putInt("maxPoints", maxQuestionNumber+1);
+            intent.putExtras(b2);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/biennale_go/QuizSummaryActivity.java
+++ app/src/main/java/com/example/biennale_go/QuizSummaryActivity.java
@@ -4,22 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class QuizSummaryActivity extends AppCompatActivity {
-    private Button repeatButton, exitButton;
+    private Button exitButton;
+    private TextView pointsTextView;
+    private Bundle b;
+    private Integer points;
+    private Integer maxPoints;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_summary);
-        repeatButton = (Button) findViewById(R.id.repeatButton);
-        repeatButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openQuizActivity();
-            }
-        });
+        pointsTextView = (TextView) findViewById(R.id.pointsTextView);
         exitButton = (Button) findViewById(R.id.exitButton);
         exitButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,11 +29,16 @@ public class QuizSummaryActivity extends AppCompatActivity {
                 openMainActivity();
             }
         });
+
+        b = getIntent().getExtras();
+
+        if (b != null) {
+            points =  b.getInt("points");
+            maxPoints =  b.getInt("maxPoints");
+            pointsTextView.setText(points + "/" + maxPoints);
+        }
     }
-    public void openQuizActivity(){
-        Intent intent = new Intent(this, QuizActivity.class);
-        startActivity(intent);
-    }
+
     public void openMainActivity(){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
