43e0e79ef5ab0e9f7879b15397a6508433c9deee
damian_zalewskipl@o2.pl <damian_zalewskipl@o2.pl>
2019-09-06 13:47:51 +0200

    poi details refactor

--- app/src/main/java/com/example/biennale_go/Fragments/PoiDetailsFragment.java
+++ app/src/main/java/com/example/biennale_go/Fragments/PoiDetailsFragment.java
@@ -2,36 +2,28 @@ package com.example.biennale_go.Fragments;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.media.Image;
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.example.biennale_go.MapsActivity;
 import com.example.biennale_go.R;
-
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
 
 public class PoiDetailsFragment extends Fragment {
     private String name, image, address, description;
     private Boolean checked;
     private TextView nameTextView, addressTextView, descriptionTextView;
     private Bundle b;
-    private ImageView PoiImageView, checkedImageView;
-    private ImageView showOnMapButton;
+    private ImageView PoiImageView, checkedImageView, showOnMapButton;
     private View view;
 
     @Override
@@ -45,6 +37,7 @@ public class PoiDetailsFragment extends Fragment {
             address = (String) b.getString("address");
             description = (String) b.getString("description");
             checked = (Boolean) b.getBoolean("checked");
+
             nameTextView = (TextView) view.findViewById(R.id.nameTextView);
             nameTextView.setText(name);
             addressTextView = (TextView) view.findViewById(R.id.addressTextView);
@@ -53,11 +46,11 @@ public class PoiDetailsFragment extends Fragment {
             descriptionTextView.setText(description);
             checkedImageView = (ImageView) view.findViewById(R.id.checkedImageView);
             checkedImageView.setImageResource(R.drawable.checked);
-            Log.d("check test", checked ? "true" : "false");
+
             if (checked) checkedImageView.setVisibility(View.VISIBLE);
-            StrictMode.ThreadPolicy policy = new
-                    StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
+
             PoiImageView = (ImageView) view.findViewById(R.id.poiImageView);
             URL url = null;
             try {
