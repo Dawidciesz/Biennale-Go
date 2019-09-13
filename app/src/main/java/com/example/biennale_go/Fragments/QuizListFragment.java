c0f041ec574d57099a5e675606d00246d4ce58e4
damian_zalewskipl@o2.pl <damian_zalewskipl@o2.pl>
2019-09-13 13:11:50 +0200

    fixes no data problem - quizzes

--- app/src/main/java/com/example/biennale_go/Fragments/QuizListFragment.java
+++ app/src/main/java/com/example/biennale_go/Fragments/QuizListFragment.java
@@ -34,9 +34,11 @@ import java.util.Map;
 public class QuizListFragment extends Fragment {
     private LinearLayout quizListPanel;
     private Button newButton;
-    private ArrayList scoresList;
-    private  View view;
+    private View view;
     private String id = CurrentUser.uId;
+    private ArrayList questions = new ArrayList(), quizzesNames = new ArrayList(), scoresList = new ArrayList();
+    private Map<String, Integer> scores = new HashMap<String, Integer>();
+    private Map<String, Object> quizData = new HashMap<>();
     private static final String TAG = "QuizListActicity";
 
     @Override
@@ -44,8 +46,7 @@ public class QuizListFragment extends Fragment {
         view = inflater.inflate(R.layout.activity_quiz_list, container, false);
         super.onCreate(savedInstanceState);
         quizListPanel = (LinearLayout) view.findViewById(R.id.quizListPanel);
-        scoresList = new ArrayList();
-        fetchScores();
+        fetchQuizzes();
         return view;
     }
 
@@ -56,7 +57,6 @@ public class QuizListFragment extends Fragment {
         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                Map<String, Integer> scores = new HashMap<String, Integer>();
                 if (task.isSuccessful()) {
                     DocumentSnapshot document = task.getResult();
                     if (document.exists()) {
@@ -69,86 +69,113 @@ public class QuizListFragment extends Fragment {
                             scores.put(name, score);
                         }
                     } else {
-                        Log.d(TAG, "No such document");
+                        Map<String, Object> data = new HashMap<>();
+                        ArrayList scoresArray = new ArrayList();
+                        for(Integer i = 0; i<quizzesNames.size(); i++) {
+                            Map<String,String> tempMap = new HashMap<>();
+                            tempMap.put("name", quizzesNames.get(i).toString());
+                            tempMap.put("score","0");
+                            scoresArray.add(tempMap);
+                        }
+                        scoresList = scoresArray;
+                        for(int i=0; i<scoresList.size(); i++) {
+                            Map<String, String> temp = new HashMap<String, String>();
+                            temp.putAll((HashMap<String,String>)scoresList.get(i));
+                            String name = temp.get("name");
+                            Integer score = Integer.parseInt(temp.get("score"));
+                            scores.put(name, score);
+                        }
+                        data.put("scores",scoresArray);
+                        db.collection("quizzes_scores").document(CurrentUser.uId).set(data);
                     }
+                    Log.d("scorestest", scores.toString());
+                    addButtons();
                 } else {
                     Log.d(TAG, "get failed with ", task.getException());
                 }
-                fetchQuizzes(scores);
             }
         });
     }
 
-    public void fetchQuizzes(final Map<String, Integer> scores) {
+    public void fetchQuizzes() {
         final FirebaseFirestore db = FirebaseFirestore.getInstance();
         CollectionReference docRef = db.collection("quizzes");
         docRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                view.findViewById(R.id.loadingPanel).setVisibility(View.GONE);
                 if (task.isSuccessful()) {
                     for (final QueryDocumentSnapshot document : task.getResult()) {
-                        // generate new button with quizzes data
-                        final ArrayList questions = (ArrayList) document.getData().get("questions");
-                        newButton = new Button(getContext());
-                        newButton.setText(document.getData().get("name").toString());
-                        newButton.setClickable(true);
-                        newButton.setGravity(Gravity.CENTER);
-                        newButton.setBackgroundColor(Color.parseColor("#ffffff"));
-                        newButton.setTextColor(Color.parseColor("#00574b"));
-                        newButton.setPadding(10,0,10,0);
-                        newButton.setOnClickListener(new View.OnClickListener()
-                        {
-                            @Override
-                            public void onClick(View v) {
-                                Bundle b = new Bundle();
-                                b.putString("name", document.getData().get("name").toString());
-                                b.putSerializable("questions", questions);
-                                Integer key = 0;
-                                for(Integer i = scores.size(); i>=0; i--) {
-                                    if(scores.get(document.getData().get("name").toString()) != null) break;
-                                    key++;
-                                }
-                                b.putInt("key", key);
-                                b.putSerializable("scoresList", scoresList);
-                                Fragment testFragment = new QuizFragment();
-                                testFragment.setArguments(b);
-                                FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
-                                fragmentTransaction.replace(R.id.fragment_container, testFragment);
-                                fragmentTransaction.commit();
-                            }
-                        });
-                        Drawable img = ContextCompat.getDrawable(getContext(), R.drawable.award);
-                        Double questionsCount = 0.0;
-                        if(questions != null) {
-                            questionsCount = Double.valueOf(questions.size());
-                        }
-                            if(scores.get(document.getData().get("name").toString()) != null) {
-                                Double score = Double.valueOf(scores.get(document.getData().get("name").toString()));
-                                if(score != 0 && questionsCount != 0) {
-                                    Double percentage = (score/questionsCount) * 100;
-                                    if(percentage >= 80.0) {
-                                        img =   ContextCompat.getDrawable(getContext(), R.drawable.awardgold );
-                                    } else if(percentage >= 50.0) {
-                                        img =   ContextCompat.getDrawable(getContext(), R.drawable.awardsilver );
-                                    } else if(percentage >= 30.0) {
-                                        img =  ContextCompat.getDrawable(getContext(), R.drawable.awardbronze );
-                                    }
-                                }
-                            }
-                        img.setBounds( 0, 0, 60, 60 );
-                        newButton.setCompoundDrawables( img, null, img, null );
-                        quizListPanel.addView(newButton);
-                        newButton = new Button(getContext());
-                        newButton.setVisibility(View.INVISIBLE);
-                        quizListPanel.addView(newButton);
+                        quizzesNames.add(document.getData().get("name").toString());
+                        quizData.put(document.getData().get("name").toString(), (ArrayList) document.getData().get("questions"));
                     }
+                    fetchScores();
                 } else {
                     Log.d(TAG, "Error getting documents: ", task.getException());
                 }
-                view.findViewById(R.id.quizListPanel).setVisibility(View.VISIBLE);
             }
         });
+    }
+
+    public void addButtons() {
+        for(Integer i  = 0; i<quizzesNames.size(); i++) {
+            newButton = new Button(getContext());
+
+            final Integer j = i;
+            newButton.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v) {
+                    Bundle b = new Bundle();
+                    b.putString("name", quizzesNames.get(j).toString());
+                    if(quizData.get(quizzesNames.get(j).toString()) != null) {
+                        b.putSerializable("questions", (ArrayList) quizData.get(quizzesNames.get(j).toString()));
+                    }
+                    ArrayList scoresList = new ArrayList();
+                    scoresList.add(scores);
+                    b.putSerializable("scoresList", scoresList);
+                    Fragment testFragment = new QuizFragment();
+                    testFragment.setArguments(b);
+                    FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.fragment_container, testFragment);
+                    fragmentTransaction.commit();
+                }
+            });
+
+            newButton.setText(quizzesNames.get(j).toString());
+            newButton.setClickable(true);
+            newButton.setGravity(Gravity.CENTER);
+            newButton.setBackgroundColor(Color.parseColor("#ffffff"));
+            newButton.setTextColor(Color.parseColor("#00574b"));
+            newButton.setPadding(10,0,10,0);
+
+            Drawable img = ContextCompat.getDrawable(getContext(), R.drawable.award);
+            Double questionsCount = 0.0;
+            if(quizData.get(quizzesNames.get(j).toString()) != null) {
+                    ArrayList tempArray = (ArrayList) quizData.get(quizzesNames.get(j).toString());
+                    questionsCount = Double.valueOf(tempArray.size());
+            }
+            if(scores.get(quizzesNames.get(j).toString()) != null) {
+                Double score = Double.valueOf(scores.get(quizzesNames.get(j).toString()));
+                if(score != 0 && questionsCount != 0) {
+                    Double percentage = (score/questionsCount) * 100;
+                    if(percentage >= 80.0) {
+                        img =   ContextCompat.getDrawable(getContext(), R.drawable.awardgold );
+                    } else if(percentage >= 50.0) {
+                        img =   ContextCompat.getDrawable(getContext(), R.drawable.awardsilver );
+                    } else if(percentage >= 30.0) {
+                        img =  ContextCompat.getDrawable(getContext(), R.drawable.awardbronze );
+                    }
+                }
+            }
+            img.setBounds( 0, 0, 60, 60 );
+            newButton.setCompoundDrawables( img, null, img, null );
+            quizListPanel.addView(newButton);
+            newButton = new Button(getContext());
+            newButton.setVisibility(View.INVISIBLE);
+            quizListPanel.addView(newButton);
+        }
+        view.findViewById(R.id.loadingPanel).setVisibility(View.GONE);
+        view.findViewById(R.id.quizListPanel).setVisibility(View.VISIBLE);
     }
 }
